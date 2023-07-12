8b96dde57a967d954aeb1f034aa69f4e64943f0e
guillemontecinos <gamontecin@gmail.com>
2023-07-12 19:59:18 -0400

    system working with sensor reading, time syncing, server detecting disconnections and plot displaying

--- arduino-sketch/dht22-http-post/dht22-http-post.ino
+++ arduino-sketch/dht22-http-post/dht22-http-post.ino
@@ -4,22 +4,25 @@
 #include "secrets.h"
 
 // Wifi settings ============================
-
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
-
-char serverAddress[] = "192.168.2.155";
-int port = 8000;
-
+char serverAddress[] = "192.168.3.142";
 WiFiClient wifi;
-HttpClient client = HttpClient(wifi, serverAddress, port);
 int status = WL_IDLE_STATUS;
+
+// HTTP settings ============================
+int port = 8000;
+HttpClient client = HttpClient(wifi, serverAddress, port);
+bool timeTracked = false;
 
 // DHT settings ============================
 #define DHTPIN 2
 #define DHTTYPE DHT22
-
 DHT dht(DHTPIN, DHTTYPE);
+
+// System variables ========================
+int serverInitialTime = 0;
+int localInitialTime = 0;
 
 void setup() {
   client.setTimeout(1);
@@ -49,39 +52,56 @@ void setup() {
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
-
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.println();
-
   // POST Request
-  Serial.println("making POST request");
   String contentType = "application/x-www-form-urlencoded";
-  String postData = "temperature=" + String(t) + "&humidity=" + String(h);
+  if(!timeTracked){
+    Serial.println("syncing timestamp");
+    delay(1000);
+    // POST Request
+    String postData = "";
+    client.post("/sync", contentType, postData);
+    String response = client.responseBody();
+    // Maybe also eval if response.length is large
+    if(response != "-2"){
+      serverInitialTime = response.toInt();
+      localInitialTime = millis();
+      timeTracked = true;
+      Serial.print("Server Init time: ");
+      Serial.print(serverInitialTime);
+      Serial.println();
+    }
+  }
+  else{
+    // Wait a few seconds between measurements.
+    delay(2000);
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
 
-  client.post("/test", contentType, postData);
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
 
-  // read the status code and body of the response
-  int statusCode = client.responseStatusCode();
-  String response = client.responseBody();
+    Serial.print(F("Humidity: "));
+    Serial.print(h);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(t);
+    Serial.print(F("°C "));
+    Serial.println();
 
-  Serial.print("Status code: ");
-  Serial.println(statusCode);
-  Serial.print("Response: ");
-  Serial.println(response);
+    // POST Request
+    String postData = "temperature=" + String(t) + "&humidity=" + String(h) + "&timestamp=" + String(serverInitialTime + millis() - localInitialTime);
+    client.post("/test", contentType, postData);
+    // read the status code and body of the response
+    int statusCode = client.responseStatusCode();
+    String response = client.responseBody();
+
+    Serial.print("Status code: ");
+    Serial.println(statusCode);
+    Serial.print("Response: ");
+    Serial.println(response);
+  }
 }
--- server/index.js
+++ server/index.js
@@ -13,22 +13,29 @@ app.get('/scope', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-// app.post('/test', (req, res) => {
-//     console.log('post request received')
-//     console.log(req.body)
-//     res.sendStatus(200)
-// })
+app.post('/sync', (req, res) => {
+    res.send(Date.now().toString())
+})
+
+app.post('/test', (req, res) => {
+    console.log('post request received')
+    console.log(req.body)
+    res.sendStatus(200)
+    sockets.forEach(s => {
+        s.send(JSON.stringify(req.body))
+    })
+})
 
 app.listen(port, () => {
     console.log('app running at port: ' + port)
 })
 
+let sockets = []
+
 function handleWs(ws){
-    app.post('/test', (req, res) => {
-        console.log('post request received')
-        console.log(req.body)
-        res.sendStatus(200)
-        ws.send(JSON.stringify(req.body))
+    sockets.push(ws)
+    ws.on('close', () => {
+        sockets.splice(sockets.findIndex(s => s == ws))
     })
 }
 
